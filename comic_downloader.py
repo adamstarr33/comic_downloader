15f061d56c53cedc9bebdda4ae0547d025ba16ef
Adam Starr <adamstarr33@gmail.com>
2021-09-03 16:34:19 -0400

    first draft of one issue vs entire series functionality

--- comic_downloader.py
+++ comic_downloader.py
@@ -5,8 +5,8 @@ options.headless = True
 driver = webdriver.Firefox(options = options, executable_path= r"C:\Users\starr\Documents\Dev\Web_Drivers\geckodriver.exe")
 import bs4, os, sys, urllib.request
 
-custom_path = input("Want to save to a custom path? ")
-if custom_path == "y" or "Y":
+custom_path = str(input("Want to save to a custom path? "))
+if custom_path == "y" or custom_path == "Y":
     parentdir = input("Please enter the path you want to save to ")
 else:
     parentdir = 'C:/Users/starr/Documents/Comics'
@@ -24,9 +24,10 @@ else:
     url = input("Enter a comic link pls: ")
     print(url)
 
-if every_issue == 1:
-    print('Downloading page %s...' % url)
+if every_issue == "1":
+    url = url+"/Issue-1"
     res = driver.get(url)
+    print('Downloading page %s...' % url)
     soup = bs4.BeautifulSoup(driver.page_source, features="lxml")
     subject_options = soup.findAll('select', attrs = {'id': 'selectPage'} )[0].findAll("option")
     for i in range(len(subject_options)-1):
@@ -49,12 +50,12 @@ else:
     for i in range(len(num_issues)-1):
         os.mkdir(path+"/"+str(i+1))
         num_pages = soup.findAll('select', attrs = {'id': 'selectPage'} )[0].findAll("option")
-        for i in range(len(num_pages)-1):
+        for j in range(len(num_pages)-1):
             comicElem = driver.find_element_by_css_selector("#imgCurrent")
             comicUrl = comicElem.get_attribute("src")
             print('Downloading image %s...' % (comicUrl))
-            urllib.request.urlretrieve(comicUrl, path+"/" + str(i+1) + ".png")
-            print('Downloading page %s...' % (url+"#"+str(i+2)))
+            urllib.request.urlretrieve(comicUrl, path+"/" + str(i+1) + ".cbr")
+            print('Downloading page %s...' % (url+"/"+str(i+1)+"#"+str(i+2)))
             newurl = url+"#"+str(i+2)
             print(newurl)
             driver.close()
