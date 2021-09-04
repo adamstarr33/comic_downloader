d75e7b0d6c6a5d1cca6278dd6bdf1a750ae0b415
Adam Starr <adamstarr33@gmail.com>
2021-09-04 10:18:37 -0400

    added first draft of ability to start from specific issue and page

--- comic_downloader.py
+++ comic_downloader.py
@@ -3,7 +3,7 @@ from selenium.webdriver.firefox.options import Options
 options = Options()
 options.headless = True
 driver = webdriver.Firefox(options = options, executable_path= r"C:\Users\starr\Documents\Dev\Web_Drivers\geckodriver.exe")
-import bs4, os, sys, urllib.request
+import bs4, os, sys, urllib.request, time, progressbar
 
 custom_path = str(input("Want to save to a custom path? "))
 if custom_path == "y" or custom_path == "Y":
@@ -15,7 +15,7 @@ path = os.path.join(parentdir, dir)
 os.mkdir(path)
 print("Directory %s created" % path)
 
-every_issue = input("Press 1 to download just one issue or 2 to download the entire series ")
+every_issue = input("Press 1 to download just one issue, 2 to download the entire series, 3 to download the series starting from a certain spot ")
 
 if len(sys.argv) > 1:
     url = sys.argv[1]
@@ -42,16 +42,16 @@ if every_issue == "1":
         driver = webdriver.Firefox(options = options, executable_path= r"C:\Users\starr\Documents\Dev\Web_Drivers\geckodriver.exe")
         res = driver.get(newurl)
     driver.close()
-else:
+if every_issue == "2":
     new_url = url+"/Issue-1"
     print('Downloading page %s...' % new_url)
     os.mkdir(path+"/1")
     res = driver.get(new_url)
     soup = bs4.BeautifulSoup(driver.page_source, features="lxml")
     num_issues = soup.findAll('select', attrs = {'id': 'selectEpisode'} )[0].findAll("option")
-    for i in range(len(num_issues)):
+    for i in progressbar.progressbar(range(len(num_issues))):
         num_pages = soup.findAll('select', attrs = {'id': 'selectPage'} )[0].findAll("option")
-        for j in range(len(num_pages)-1):
+        for j in progressbar.progressbar(range(len(num_pages))):
             comicElem = driver.find_element_by_css_selector("#imgCurrent")
             comicUrl = comicElem.get_attribute("src")
             print('Downloading image %s...' % (comicUrl))
@@ -69,3 +69,34 @@ else:
         soup = bs4.BeautifulSoup(driver.page_source, features="lxml")
     driver.close()
 
+if every_issue == "3":
+    issue = input("What issue? ")
+    page = input("What page? ")
+    new_url = url+"/Issue-%s#%s" %(issue, page)
+    print('Downloading page %s...' % new_url)
+    os.mkdir(path+"/%s" % issue)
+    res = driver.get(new_url)
+    soup = bs4.BeautifulSoup(driver.page_source, features="lxml")
+    num_issues = soup.findAll('select', attrs = {'id': 'selectEpisode'} )[0].findAll("option")
+    print("num issues is %s" %len(num_issues))
+    for i in progressbar.progressbar(range(int(issue), len(num_issues)+1)):
+        num_pages = soup.findAll('select', attrs = {'id': 'selectPage'} )[0].findAll("option")
+        for j in progressbar.progressbar(range(int(page), len(num_pages)+1)):
+            comicElem = driver.find_element_by_css_selector("#imgCurrent")
+            comicUrl = comicElem.get_attribute("src")
+            print('Downloading image %s...' % (comicUrl))
+            print(path)
+            urllib.request.urlretrieve(comicUrl, path+ "/" + str(i) + "/" + str(j) + ".png")
+            print('Downloading page %s...' % (url+"/Issue-%s#%s" %(str(i), str(j))))
+            newurl = url+"/Issue-%s#%s" %(str(i), str(j))
+            print(newurl)
+            driver.close()
+            driver = webdriver.Firefox(options = options, executable_path= r"C:\Users\starr\Documents\Dev\Web_Drivers\geckodriver.exe")
+            res = driver.get(newurl)
+        #driver.close()
+        new_url = url+"/Issue-" + str(i+1)
+        print(new_url)
+        os.mkdir(path+"/"+str(i+1))
+        res = driver.get(new_url)
+        soup = bs4.BeautifulSoup(driver.page_source, features="lxml")
+    driver.close()
