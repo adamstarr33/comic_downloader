82b5e6f208b1dbab1f3bae2ebcf18331dcca52f7
Adam Starr <adamstarr33@gmail.com>
2021-09-03 22:48:20 -0400

    one isue works, full series is getting there

--- comic_downloader.py
+++ comic_downloader.py
@@ -30,7 +30,7 @@ if every_issue == "1":
     print('Downloading page %s...' % url)
     soup = bs4.BeautifulSoup(driver.page_source, features="lxml")
     subject_options = soup.findAll('select', attrs = {'id': 'selectPage'} )[0].findAll("option")
-    for i in range(len(subject_options)-1):
+    for i in range(len(subject_options)):
         comicElem = driver.find_element_by_css_selector("#imgCurrent")
         comicUrl = comicElem.get_attribute("src")
         print('Downloading image %s...' % (comicUrl))
@@ -43,24 +43,29 @@ if every_issue == "1":
         res = driver.get(newurl)
     driver.close()
 else:
-    print('Downloading page %s...' % url)
-    res = driver.get(url)
+    new_url = url+"/Issue-1"
+    print('Downloading page %s...' % new_url)
+    os.mkdir(path+"/1")
+    res = driver.get(new_url)
     soup = bs4.BeautifulSoup(driver.page_source, features="lxml")
     num_issues = soup.findAll('select', attrs = {'id': 'selectEpisode'} )[0].findAll("option")
-    for i in range(len(num_issues)-1):
-        os.mkdir(path+"/"+str(i+1))
+    for i in range(len(num_issues)):
         num_pages = soup.findAll('select', attrs = {'id': 'selectPage'} )[0].findAll("option")
         for j in range(len(num_pages)-1):
             comicElem = driver.find_element_by_css_selector("#imgCurrent")
             comicUrl = comicElem.get_attribute("src")
             print('Downloading image %s...' % (comicUrl))
-            urllib.request.urlretrieve(comicUrl, path+"/" + str(i+1) + ".cbr")
-            print('Downloading page %s...' % (url+"/"+str(i+1)+"#"+str(i+2)))
-            newurl = url+"#"+str(i+2)
+            urllib.request.urlretrieve(comicUrl, path+ "/" + str(i+1) + "/" + str(j+1) + ".png")
+            print('Downloading page %s...' % (url+"#"+str(j+2)))
+            newurl = new_url+"#"+str(j+2)
             print(newurl)
             driver.close()
             driver = webdriver.Firefox(options = options, executable_path= r"C:\Users\starr\Documents\Dev\Web_Drivers\geckodriver.exe")
             res = driver.get(newurl)
-        
+        driver.close()
+        new_url = url+"/Issue-" + str(i+2)
+        os.mkdir(path+"/"+str(i+2))
+        res = driver.get(new_url)
+        soup = bs4.BeautifulSoup(driver.page_source, features="lxml")
     driver.close()
 
